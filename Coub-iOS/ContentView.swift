eb3fcff00890dfae62fdc6df8e73424e62a4aa7b
Sergey Lukovkin <slukovkin@icloud.com>
2024-02-23 22:33:55 +0200

    Initial Commit

---
+++ Coub-iOS/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Coub-iOS
+//
+//  Created by Sergey Lukovkin on 23.02.2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Coub-iOS/Coub_iOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  Coub_iOSApp.swift
+//  Coub-iOS
+//
+//  Created by Sergey Lukovkin on 23.02.2024.
+//
+
+import SwiftUI
+
+@main
+struct Coub_iOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
