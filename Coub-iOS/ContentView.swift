9cb28a40933cc0ebcf31802d1ce5e5bd32be84fc
Sergey Lukovkin <slukovkin@icloud.com>
2024-02-23 23:41:54 +0200

    update

--- Coub-iOS/ContentView.swift
+++ Coub-iOS/ContentView.swift
@@ -9,8 +9,40 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        ZStack {
+            LinearGradient(gradient: Gradient(colors: [Color("brown"), Color.black]), startPoint: .topLeading, endPoint: .bottomTrailing)
+                .ignoresSafeArea()
+            
+            VStack(alignment: .center, spacing: 10){
+                Text("COUB")
+                    .padding()
+                    .foregroundColor(.white)
+                    .font(.largeTitle)
+                    .padding()
+                
+                HStack {
+                    ButtonMenu(title: "Electric")
+                    ButtonMenu(title: "Water")
+                }
+                HStack {
+                    ButtonMenu(title: "Gas")
+                    ButtonMenu(title: "Report")
+                }
+                HStack(spacing: 0.0){
+                    Button(action: {}, label: {
+                        Text("Settings")
+                            .foregroundColor(.white)
+                            .font(.title)
+                            .clipShape(Capsule())
+                            .frame(width: 310, height: 100, alignment: /*@START_MENU_TOKEN@*/.center/*@END_MENU_TOKEN@*/)
+                            .border(Color.white, width: 2)
+                            .shadow(color: .black, radius: 5)
+                    })
+                }
+                
+                Spacer()
+            }
+        }
     }
 }
 
